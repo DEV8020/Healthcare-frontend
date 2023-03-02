1f9644eacc5fa7f5fdfbc3e74edbc76cf7aeb089
DEV8020 <datardevdatt8020@gmail.com>
2023-03-02 19:59:15 +0530

    had demo version 3 (doctor list UI added)

--- Web-app/src/Components/UI/Doctor/PatientList.js
+++ Web-app/src/Components/UI/Doctor/PatientList.js
@@ -1,51 +1,37 @@
-import React from 'react'
-import PatientData from './PatientData'
-import classes from './PatientList.module.css'
-
+import React from "react";
+import PatientData from "./PatientData";
+import classes from "./PatientList.module.css";
 
 const PatientList = (props) => {
-    
-    const logout = (props) => {
-        window.localStorage.removeItem('loggedInUser')
-        props.setUser(null)
-      }
-      if (!(props.user))
-      return null
-    
-    if (props.patientList === [])
-        return null
+  const logoutD = () => {
+    window.localStorage.removeItem("loggedInUser");
+    props.setUser(null);
+  };
+  if (!props.user) return null;
 
-    return (
-        <div id="patientlist">
-            <h1 >Patient Appointment List</h1>
-            <main>
-            <table>
-                <thead>
-                <tr>
-                    {/* <th>Patient ID</th> */}
-                    <th> Name</th>
-                    <th> Age</th>
-                    <th> Gender</th>
-                    <th> Phone</th>
-                </tr>
-                </thead>
-                <tbody>
-                {
-                   
-                   props.patientList.map(b =>
-                        <PatientData
-                            patientData={b}
-                            key={b.pen_id}
-                        />
-                    )
-                }
-            </tbody>
-            </table>
-            </main>
-            <button className = {classes.button} type="button"  onClick={logout}>Logout</button>
+  if (props.patientList === []) return null;
 
-        </div>
-    )
-}
+  return (
+    <div id="patientlist">
+      <h1 className={classes.head}>Patient Appointment List </h1>
 
-export default PatientList
+      <div className={classes.NavBar}>
+        <button value="logout" className={classes.logout_btn} onClick={logoutD}>
+          Log-out
+        </button>
+      </div>
+
+      <div className={classes.ul}>
+        {props.patientList.map((patientData) => (
+            <PatientData
+              patientData={patientData}
+              key={patientData.pid}
+              
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default PatientList;
